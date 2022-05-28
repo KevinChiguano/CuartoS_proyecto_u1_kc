60b558e28b76a44487754f7f513d466d9841ee48
KevinChiguano <kevin_chili1998@hotmail.com>
2022-05-27 20:18:30 -0500

    Segundo Cambio

--- src/main/java/com/uce/edu/demo/consultorio/Doctor.java
+++ src/main/java/com/uce/edu/demo/consultorio/Doctor.java
@@ -2,4 +2,35 @@ package com.uce.edu.demo.consultorio;
 
 public class Doctor {
 
+	private String nombre;
+	private String apellido;
+	private String edad;
+
+	//SET y GET
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getEdad() {
+		return edad;
+	}
+
+	public void setEdad(String edad) {
+		this.edad = edad;
+	}
+	
+	
+
 }
