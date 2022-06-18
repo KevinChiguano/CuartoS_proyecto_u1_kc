f12d3a278d81659bf4e825cc1d89892aed3c452b
KevinChiguano <kevin_chili1998@hotmail.com>
2022-06-17 20:10:21 -0500

    Lookup en Spring Framework

--- src/main/java/com/uce/edu/demo/ProyectoU1KcApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU1KcApplication.java
@@ -30,7 +30,19 @@ import com.uce.edu.demo.service.IMatriculaService;
 public class ProyectoU1KcApplication implements CommandLineRunner{
 	
 	@Autowired
-	private IReservacionService reservacionService;
+	private ProfesorGeneral general;
+	
+	@Autowired
+	private ProfesorGeneral general1;
+	
+	@Autowired
+	private ProfesorMateria materia;
+	
+	@Autowired
+	private ProfesorMateria materia1;
+	
+	@Autowired
+	private IMatriculaService iMatriculaService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU1KcApplication.class, args);
@@ -40,11 +52,35 @@ public class ProyectoU1KcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
+		System.out.println("EJEMPLO SINGLETON");
+		this.general.setNombre("Edison");
+		this.general.setApellido("Cayambe");
 		
-		System.out.println( this.reservacionService.realizarReservacion("002"));
+		System.out.println(this.general);
+		System.out.println("--------");
+		System.out.println(this.general1);
+		this.general1.setNombre("Pepito");
+		System.out.println("--------");
+		System.out.println(this.general);
+		
+		System.out.println("--------");
+		System.out.println(this.general1);
+		
+		System.out.println("EJEMPLO PROTOTYPE");
+		this.materia.setNombre("Daniel");
+		this.materia.setApellido("Teran");
+		System.out.println(this.materia);
+		System.out.println("--------");
+		System.out.println(this.materia1);
+		
 		System.out.println("----------");
-		System.out.println( this.reservacionService.realizarReservacion("111"));
-	
+		Matricula matricula1 = new Matricula();
+		matricula1.setEstudiante(new Estudiante());
+		matricula1.setMateria(new ArrayList<Materia>());
+		matricula1.setNumero("1234");
+		
+		this.iMatriculaService.crearMatricula(matricula1);
+		
 	}
 
 }
--- src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
+++ src/main/java/com/uce/edu/demo/service/MatriculaServiceImpl.java
@@ -1,6 +1,7 @@
 package com.uce.edu.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
 import org.springframework.stereotype.Service;
 
 import com.uce.edu.demo.modelo.Matricula;
@@ -20,11 +21,31 @@ public class MatriculaServiceImpl implements IMatriculaService{
 	@Autowired
 	private ProfesorMateria profesorMateria;
 	
+	@Lookup
+	public ProfesorGeneral obtenerProfesorG() {
+		return null;
+	}
+	
+	@Lookup
+	public ProfesorMateria obtenerProfesorM() {
+		//Se crea momentaneamente y luego se descarta
+		ProfesorMateria profeM = new ProfesorMateria();
+		profeM.setApellido("Tapia");
+		profeM.setApellido("Jose");
+		return profeM;
+	}
+	
 	@Override
 	public void crearMatricula(Matricula ma) {
 		// TODO Auto-generated method stub
+		
+		//ProfesorMateria profe = this.profesorMateria;
+		
 		System.out.println("DI desde Service SINGLETON " + this.profesorGeneral);
 		System.out.println("DI desde Service PROTOTYPE " + this.profesorMateria);
+		System.out.println("DI un metodo General: " + this.obtenerProfesorG());
+		System.out.println("DI un metodo Materia: " + this.obtenerProfesorM());
+		
 		this.imatriculaRepository.insertar(ma);
 		
 	}
